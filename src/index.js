a265945aaeee193bd1b5a695ba4e5ba21fb4ff3f
crisyg97 <cristiangaray101@gmail.com>
2023-10-05 12:03:07 -0300

    added routes and navigation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ejs from "ejs";
+import {router} from "../routes/index.js"
 
 import {dirname} from "path";     //->usados para obtener ruta absoluta de forma dinamica de las vistas
 import {fileURLToPath} from "url";//--^
@@ -11,10 +12,7 @@ const app = express()
 
 app.set(__dirname,"/views",) //indico donde esta la carpeta de las vistas
 app.set("view engine","ejs")
-
-app.get("/",(req, res) => res.render("index.ejs"))
-app.get("/about",(req, res) => res.render("about"))
-app.get("/contact",(req, res) => res.render("contact"))
+app.use(router)
 
 app.listen(3000)
 console.log("server is listening on port", 3000)
