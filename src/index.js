4d43ad6800cee73b31e4a9177f5c77085f769fc3
crisyg97 <cristiangaray101@gmail.com>
2023-10-05 11:09:17 -0300

    added views

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import ejs from "ejs";
 
+import {dirname} from "path";     //->usados para obtener ruta absoluta de forma dinamica de las vistas
+import {fileURLToPath} from "url";//--^
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) //ruta absoluta
+console.log(__dirname,'/views')
+
 const app = express()
-app.get("/",(req, res) => res.send("hello server"))
+
+app.set(__dirname,"/views",) //indico donde esta la carpeta de las vistas
+app.set("view engine","ejs")
+
+app.get("/",(req, res) => res.render("index.ejs"))
+app.get("/about",(req, res) => res.render("about"))
+app.get("/contact",(req, res) => res.render("contact"))
 
 app.listen(3000)
 console.log("server is listening on port", 3000)
