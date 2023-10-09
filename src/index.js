bd3f2d38186e82c5f1f7a8fe25c91a1160050671
crisyg97 <cristiangaray101@gmail.com>
2023-10-09 10:46:40 -0300

    simple views added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import ejs from "ejs";
-import {router} from "../routes/index.js"
+import {router} from "./routes/index.js"
 
 import {dirname} from "path";     //->usados para obtener ruta absoluta de forma dinamica de las vistas
 import {fileURLToPath} from "url";//--^
 
-const __dirname = dirname(fileURLToPath(import.meta.url)) //ruta absoluta
-console.log(__dirname,'/views')
+const __dirname = dirname(fileURLToPath(import.meta.url)) //ruta absoluta de src 
+//console.log(__dirname,'/views')
 
 const app = express()
 
 app.set(__dirname,"/views",) //indico donde esta la carpeta de las vistas
 app.set("view engine","ejs")
 app.use(router)
+
+app.use(express.static(__dirname,'/public',))
 
 app.listen(3000)
 console.log("server is listening on port", 3000)
